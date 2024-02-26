05491198eb4e120768ff83d8cafc60ab6d6b26a5
Sarvanan N <nsaravanan1204@gmail.com>
2024-02-26 13:28:16 +0530

    config added

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,10 @@ module.exports ={
         path:path.join(__dirname,'public'),
         filename: 'bundle.js'
     },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+    
     module: {
         rules: [{
-                test: /\.(js|jsx)$/,
+                test: /\.(js)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
