fb445629c76e4cd3e76a19883d8d269ea37615c3
Sarvanan N <nsaravanan1204@gmail.com>
2024-02-26 11:14:02 +0530

    webpack config

--- src/Counter.js
+++ src/Counter.js
@@ -2,13 +2,45 @@ import React from "react";
 
 
 export class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {count: 0};
+    }
+    handleAddOne(){
+        //this.state.count = this.state.count +1;
+        this.setState((prevStat)=>{
+            return {
+                count:prevStat.count+1
+            }
+        })
+        console.log('handleAddOne',this.state);
+    }
+    handleMinusOne(){
+        this.setState((prevStat)=>{
+            return {
+                count:prevStat.count-1
+            }
+        })
+        console.log('handleMinusOne');
+    }
+    handleReset(){
+        this.setState((prevStat)=>{
+            return {
+                count:0
+            }
+        })
+        console.log('handleReset');
+    }
     render(){
         return(
             <div>
-                <h1>Count: </h1>
-                <button>+1</button>
-                <button>-1</button>
-                <button>reset</button>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
             </div>
         )
     }
